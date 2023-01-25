6852f10277b617d8734ffad4cbe4dde2816759d6
emonShimoul <emonshimoul@gmail.com>
2023-01-26 00:44:58 +0600

    remove unnecessary comments

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -6,7 +6,7 @@ const ProfileCard = ({ title, handle, description }) => {
         <div className='card'>
             <div className="card-image">
                 <figure className='image is-1by1'>
-                    <img src="https://media.licdn.com/dms/image/C4D03AQGWq6ENYLpaFA/profile-displayphoto-shrink_800_800/0/1656172584936?e=1680134400&v=beta&t=HJi4-F0vjS-ossaBOTnH6Btq2hEedOQd5AHW0EKlIlQ" alt="image" />
+                    <img src="https://media.licdn.com/dms/image/C4D03AQGWq6ENYLpaFA/profile-displayphoto-shrink_800_800/0/1656172584936?e=1680134400&v=beta&t=HJi4-F0vjS-ossaBOTnH6Btq2hEedOQd5AHW0EKlIlQ" alt="pictures" />
                 </figure>
             </div>
 
