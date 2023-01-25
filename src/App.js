4fdabadbfae65927bdef23f5e4ea860660f72e59
emonShimoul <emonshimoul@gmail.com>
2023-01-26 00:31:17 +0600

    use bulma css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import ProfileCard from './components/ProfileCard/ProfileCard';
 function App() {
   return (
     <div className="App">
-      <ProfileCard title="Emon Shimoul" handle="CSE"></ProfileCard>
-      <ProfileCard title="Farah Iffat" handle="BBA"></ProfileCard>
-      <ProfileCard title="Maahmood Abeer" handle="EEE"></ProfileCard>
+      <div>Personal Digital Assistants</div>
+      <div className="container">
+        <section className='section'>
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Emon Shimoul" handle="CSE"></ProfileCard>
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Farah Iffat" handle="BBA"></ProfileCard>
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Maahmood Abeer" handle="EEE"></ProfileCard>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -3,9 +3,19 @@ import React from 'react';
 const ProfileCard = ({ title, handle }) => {
     // const { title, handle } = props;
     return (
-        <div>
-            <div>This is {title}</div>
-            <div>This is {handle}</div>
+        <div className='card'>
+            <div className="card-image">
+                <figure className='image is-1by1'>
+                    <img src="https://media.licdn.com/dms/image/C4D03AQGWq6ENYLpaFA/profile-displayphoto-shrink_800_800/0/1656172584936?e=1680134400&v=beta&t=HJi4-F0vjS-ossaBOTnH6Btq2hEedOQd5AHW0EKlIlQ" alt="image" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className='title is-4'>{title}</p>
+                    <p className='subtitle is-6'>{handle}</p>
+                </div>
+            </div>
         </div>
     );
 };
